ea5c97fb8317bce7204bb2f5d5b80607207b545c
Sayak Bhattacharya <Sayak Bhattacharya>
2017-07-16 11:41:54 +0530

    second commit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var methodOverride = require('method-override');
 
 
 app.use(express.static('./public'));
+app.use(express.static('./bower_components'));
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
